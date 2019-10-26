6dbc2ff0140284e7a9db6d58269a584e5b766303
RomanBuletsa <r.buletsa@gmail.com>
2019-10-27 02:51:30 +0300

    Added users page

---
+++ Assets/Scripts/UsersPage/UserView.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using Data;
+using TMPro;
+using UnityEngine;
+using UnityEngine.UI;
+
+namespace UsersPage
+{
+    public sealed class  UserView : MonoBehaviour
+    {
+        public event Action<UserData> UserButtonClicked;
+        [SerializeField] private TMP_Text nickname;
+        [SerializeField] private TMP_Text city;
+        [SerializeField] private TMP_Text spent;
+        [SerializeField] private Button userButton;
+
+        public void Show(UserData userData)
+        {
+            nickname.text = userData.nickname;
+            city.text = userData.city;
+            spent.text = userData.spent.ToString();
+            userButton.onClick.AddListener(() => UserButtonClicked?.Invoke(userData));
+        }
+    }
+}
