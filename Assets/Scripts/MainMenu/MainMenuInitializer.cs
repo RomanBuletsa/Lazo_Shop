a495da35b2c81fc7defe4850f61107c743e72aae
Vitaliy Lazorishinets <advancerp13@gmail.com>
2019-12-20 00:53:16 +0200

    Added fonts + make start menu window

--- Assets/Scripts/MainMenu/MainMenuInitializer.cs
+++ Assets/Scripts/MainMenu/MainMenuInitializer.cs
@@ -2,13 +2,20 @@
 using Data;
 using SceneManagement;
 using UnityEngine;
+using UnityEngine.UI;
 
 namespace MainMenu
 {
     public sealed class MainMenuInitializer : BaseInitializer
     {
+        [SerializeField] private Button adminPageBtn;
+        [SerializeField] private Button userPageBtn;
+        [SerializeField] private Button statisticsPageBtn;
         protected override void Init()
         {
+            adminPageBtn.onClick.AddListener(() => ScenesLoader.LoadScene(ApplicationScenes.AdminPage.ToString(), true, false));
+            userPageBtn.onClick.AddListener(()=> ScenesLoader.LoadScene(ApplicationScenes.UsersPage.ToString(), true, false));
+            statisticsPageBtn.onClick.AddListener(()=> ScenesLoader.LoadScene(ApplicationScenes.StatisticsPage.ToString(), true, false));
             var manager = new MainMenuManager();
             ApplicationManager.Instance.RegisterMainMenuManager(manager);
         }
--- Assets/Scripts/MainMenu/MainMenuManager.cs
+++ Assets/Scripts/MainMenu/MainMenuManager.cs
@@ -1,18 +1,19 @@
-﻿using Application;
+﻿using System;
+using Application;
 using Data;
 using SceneManagement;
+using UnityEditor.IMGUI.Controls;
+using UnityEngine;
+using UnityEngine.UI;
 
 namespace MainMenu
 {
     public sealed class MainMenuManager : IManager
     {
-
+        
         public void Start()
         {
-            ScenesLoader.LoadScene(ApplicationScenes.AdminPage.ToString(), true, false);
-            //ScenesLoader.LoadScene(ApplicationScenes.UsersPage.ToString(), true, false);
-            //ScenesLoader.LoadScene(ApplicationScenes.StatisticsPage.ToString(), true, false);
-            ScenesLoader.UnloadScene(ApplicationScenes.MainMenu.ToString());
+            //ScenesLoader.UnloadScene(ApplicationScenes.MainMenu.ToString());
         }
 
         public void Stop()
